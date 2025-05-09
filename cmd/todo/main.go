344eb9a1c50a7c76c77853374b74b1b196b6f871
somepgs <grishapopkov80@gmail.com>
2025-05-09 14:48:13 +0500

    A convenient CLI application interface has been implemented

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,60 +1,83 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/somepgs/todo-cli/internal/service"
 	"github.com/somepgs/todo-cli/internal/storage"
 )
 
+const usage = `Ипользование:
+	todo add "название задачи" - добавить задачу
+	todo list				   - список задач
+	todo done <id>			   - завершить задачу
+	todo delete <id>		   - удалить задачу`
+
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println(usage)
+		os.Exit(1)
+	}
+
 	store := &storage.JSONStorage{FilePath: "tasks.json"}
 	taskService := service.NewTaskService(store)
 
-	if err := taskService.Add("Прочитать документацию по Go"); err != nil {
-		log.Fatal(err)
-	}
+	switch os.Args[1] {
+	case "add":
+		addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+		addCmd.Parse(os.Args[2:])
+		title := addCmd.Arg(0)
+		if title == "" {
+			log.Fatal("Не указано название задачи")
+		}
+		if err := taskService.Add(title); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Задача добавлена!")
 
-	if err := taskService.Add("Посмотреть вебинар по архитектуре"); err != nil {
-		log.Fatal(err)
-	}
+	case "list":
+		tasks, err := taskService.List()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, task := range tasks {
+			status := " "
+			if task.Done {
+				status = "X"
+			}
+			fmt.Printf("[%s] %-3d %-30s (%s)\n", status, task.ID, task.Title, task.CreatedAt.Format("2006-01-02 15:04"))
+		}
 
-	tasks, err := taskService.List()
-	if err != nil {
-		log.Fatal(err)
-	}
+	case "done":
+		doneCmd := flag.NewFlagSet("done", flag.ExitOnError)
+		doneCmd.Parse(os.Args[2:])
+		id, err := strconv.Atoi(doneCmd.Arg(0))
+		if err != nil {
+			log.Fatal("Неправильно указан id задачи")
+		}
+		if err := taskService.Done(id); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Задача %d отмечена выполненной!\n", id)
 
-	fmt.Println("Все задачи:")
-	for _, task := range tasks {
-		fmt.Printf("[%d] %s (Выполнена: %v)\n", task.ID, task.Title, task.Done)
-	}
+	case "delete":
+		delCmd := flag.NewFlagSet("delete", flag.ExitOnError)
+		delCmd.Parse(os.Args[2:])
+		id, err := strconv.Atoi(delCmd.Arg(0))
+		if err != nil {
+			log.Fatal("Неправильно указан id задачи")
+		}
+		if err := taskService.Delete(id); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Задача %d удалена!\n", id)
 
-	if err := taskService.Done(1); err != nil {
-		log.Fatal(err)
-	}
-
-	tasks, err = taskService.List()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("\nПосле выполнения задачи с ID=1:")
-	for _, task := range tasks {
-		fmt.Printf("[%d] %s (Выполнена: %v)\n", task.ID, task.Title, task.Done)
-	}
-
-	if err := taskService.Delete(2); err != nil {
-		log.Fatal(err)
-	}
-
-	tasks, err = taskService.List()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("\nПосле удаления задачи с ID=2:")
-	for _, task := range tasks {
-		fmt.Printf("[%d] %s (Выполнена: %v)\n", task.ID, task.Title, task.Done)
+	default:
+		fmt.Println(usage)
+		os.Exit(1)
 	}
 }
